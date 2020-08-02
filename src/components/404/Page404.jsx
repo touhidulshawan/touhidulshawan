07c2785be5121efca63ba9f3625ef69f83521eff
Touhidul Shawan <touhidulshawan@gmail.com>
2020-08-02 14:19:13 +0600

    redesign of 404 page

--- src/components/404/Page404.jsx
+++ src/components/404/Page404.jsx
@@ -5,15 +5,26 @@ import { TiArrowBack } from "react-icons/ti";
 
 function Page404() {
   return (
-    <section className="">
-      <div className="">
-        <object data={img404} type="image/svg+xml">
+    <section>
+      <div className="p-4 bg-purple-500 h-64 ">
+        <object
+          className="m-auto h-full w-full "
+          data={img404}
+          type="image/svg+xml"
+        >
           404 Page Image
         </object>
       </div>
-      <Link to="/">
-        <TiArrowBack /> Back to Home
-      </Link>
+      <div className="mt-4">
+        <Link to="/">
+          <span className=" flex justify-center items-center text-sm">
+            <TiArrowBack className="text-3xl text-blue-500 mr-2" />
+            <span className="hover:text-purple-700 transition ease-out duration-500 tracking-wide uppercase text-gray-500">
+              Back to Home
+            </span>
+          </span>
+        </Link>
+      </div>
     </section>
   );
 }
