e875d7f3cd3f50f6b0dd0380cf03cd201a833b8c
Touhidul Shawan <touhidulshawan@gmail.com>
2020-09-11 21:15:10 +0600

    change dark mode according to night time

--- src/components/darkMode/useDarkMode.js
+++ src/components/darkMode/useDarkMode.js
@@ -3,6 +3,19 @@ import { useEffect, useState } from "react";
 export const useDarkMode = () => {
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    const currentTime = new Date().getHours();
+    const night = currentTime >= 19 || currentTime <= 7;
+
+    if (night) {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  }, []);
+
   const setMode = (mode) => {
     window.localStorage.setItem("theme", mode);
     setTheme(mode);
