c8d8bd3103831cd8046dacf86392dd84de974461
Touhidul Shawan <touhidulshawan@gmail.com>
2020-10-12 05:00:56 +0600

    Updates

---
+++ precache-manifest.1219ab715f395666874d0a0cbb4928fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc0dcb6319982bf353e707eca5cc81a",
+    "url": "/touhidulshawan/index.html"
+  },
+  {
+    "revision": "dbef3ebb0b1651ab168f",
+    "url": "/touhidulshawan/static/css/main.d837361a.chunk.css"
+  },
+  {
+    "revision": "a2733dac775a7d9326f5",
+    "url": "/touhidulshawan/static/js/2.55b60b13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/touhidulshawan/static/js/2.55b60b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbef3ebb0b1651ab168f",
+    "url": "/touhidulshawan/static/js/main.42ccd114.chunk.js"
+  },
+  {
+    "revision": "db5f4af9c4fefe295698",
+    "url": "/touhidulshawan/static/js/runtime-main.7006cb8c.js"
+  },
+  {
+    "revision": "a5a58a94b2c7f0d98f0918189f7f0903",
+    "url": "/touhidulshawan/static/media/404.a5a58a94.svg"
+  },
+  {
+    "revision": "e9149a6478cc97ded2ca7f8ad2204c17",
+    "url": "/touhidulshawan/static/media/programmer.e9149a64.webp"
+  }
+]);
