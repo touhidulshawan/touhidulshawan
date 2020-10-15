8abaed87593b118daf9e45fb805d215ea10bb7d7
Touhidul Shawan <touhidulshawan@gmail.com>
2020-10-16 03:05:06 +0600

    add contact feature and do some refectoring

--- src/containers/client-reviews/ReviewList.jsx
+++ src/containers/client-reviews/ReviewList.jsx
@@ -3,8 +3,8 @@ import { FaStar } from "react-icons/fa";
 
 const ReviewList = (props) => {
   return (
-    <div className="shadow-outline p-4 rounded-md">
-      <h4 className="text-purple-600 mb-1">{props.client_name}</h4>
+    <div className="p-4 rounded-md shadow-xl border-b-4 border-purple-600">
+      <h4 className="text-purple-600 mb-1 text-xl">{props.client_name}</h4>
       <div className="mb-1">
         {[...Array(props.star)].map(() => {
           return (
@@ -14,7 +14,7 @@ const ReviewList = (props) => {
           );
         })}
       </div>
-      <p className="leading-loose">{props.client_txt}</p>
+      <p className="leading-loose text-lg">{props.client_txt}</p>
     </div>
   );
 };
