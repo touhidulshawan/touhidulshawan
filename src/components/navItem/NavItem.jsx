584d82f821c7571701705134c7f735c58e716450
Touhidul Shawan <touhidulshawan@gmail.com>
2021-02-24 21:35:11 +0600

    minor edit

--- src/components/navItem/NavItem.jsx
+++ src/components/navItem/NavItem.jsx
@@ -7,7 +7,7 @@ const NavItem = (props) => {
       <NavLink
         to={props.link}
         exact
-        activeStyle={{ color: "#F59E0B" }}
+        activeStyle={{ color: "#0EA5E9" }}
         className="text-sm  uppercase tracking-wide text-purple-600 hover:text-purple-300 focus:outline-none focus:ring-2 ring-purple-600 ring-offset-2 active:text-purple-800 transform transition duration-300 ease-in-out md:text-base  md:tracking-wider dark:text-gray-300 dark:hover:text-yellow-500"
       >
         {props.linkName}
