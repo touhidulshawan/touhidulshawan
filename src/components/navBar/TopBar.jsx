76a1ce0887cc471abaa8c7ec2d94b53e80b91076
Touhidul Shawan <touhidulshawan@gmail.com>
2021-03-03 14:21:35 +0600

    update animation

--- src/components/navBar/TopBar.jsx
+++ src/components/navBar/TopBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
-import { RiMoonClearLine } from "react-icons/ri";
+// import { RiMoonClearLine } from "react-icons/ri";
+import { RiMoonLine } from "react-icons/ri";
 import { FiSun } from "react-icons/fi";
 import { useTheme, useToggleTheme } from "../../context/useThemeContext";
 
@@ -12,14 +13,14 @@ const TopBar = () => {
     <div className="p-6 flex justify-between items-center space-x-4 ">
       <div>
         {darkMode ? (
-          <RiMoonClearLine
+          <RiMoonLine
             onClick={toggleTheme}
-            className="w-6 h-6 focus:outline-none focus:ring-4 dark:text-red-400 animate-bounce cursor-pointer"
+            className="w-6 h-6 focus:outline-none focus:ring-4 dark:text-red-400 animate-pulse cursor-pointer"
           />
         ) : (
           <FiSun
             onClick={toggleTheme}
-            className="w-6 h-6 focus:outline-none focus:ring-4 text-red-600 animate-bounce cursor-pointer"
+            className="w-6 h-6 focus:outline-none focus:ring-4 text-red-600 animate-spin-slow cursor-pointer"
           />
         )}
       </div>
