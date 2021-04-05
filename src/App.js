426128dd6f8c78884de94a1f9307a18763d9ada1
Touhidul Shawan <touhidulshawan@gmail.com>
2021-04-05 18:53:33 +0600

    add redirection to  my new website address

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React from "react";
 
-import { Switch, Route, Redirect } from "react-router-dom";
+// import { Switch, Route, Redirect } from "react-router-dom";
 
-import Home from "./containers/home/Home";
-import Projects from "./containers/projects/Projects";
-import Skills from "./containers/skills/Skills";
-import Reviews from "./containers/client-reviews/ClientReviews";
-import Page404 from "./components/404/Page404";
-import { ThemeProvider } from "./context/useThemeContext";
+// import Home from "./containers/home/Home";
+// import Projects from "./containers/projects/Projects";
+// import Skills from "./containers/skills/Skills";
+// import Reviews from "./containers/client-reviews/ClientReviews";
+// import Page404 from "./components/404/Page404";
+// import { ThemeProvider } from "./context/useThemeContext";
 
-const App = () => {
-  return (
-    <ThemeProvider>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/projects" exact>
-          <Projects />
-        </Route>
-        <Route path="/skills" exact>
-          <Skills />
-        </Route>
-        <Route path="/reviews" exact>
-          <Reviews />
-        </Route>
-        <Route path="/page-not-found">
-          <Page404 />
-        </Route>
-        <Redirect to="/page-not-found" />
-      </Switch>
-    </ThemeProvider>
-  );
-};
+// const App = () => {
+//   return (
+//     <ThemeProvider>
+//       <Switch>
+//         <Route path="/" exact>
+//           <Home />
+//         </Route>
+//         <Route path="/projects" exact>
+//           <Projects />
+//         </Route>
+//         <Route path="/skills" exact>
+//           <Skills />
+//         </Route>
+//         <Route path="/reviews" exact>
+//           <Reviews />
+//         </Route>
+//         <Route path="/page-not-found">
+//           <Page404 />
+//         </Route>
+//         <Redirect to="/page-not-found" />
+//       </Switch>
+//     </ThemeProvider>
+//   );
+// };
+
+const App = () => (window.location.href = "https://touhidulshawan.vercel.app/");
 
 export default App;
